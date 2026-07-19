cba6354015b04921f6a38337ecf6f99468a877ee
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-07-19 23:54:33 +0530

    Time: 219 ms (89.87%), Space: 19.2 MB (91.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,33 +1,25 @@
-class Solution(object):
-    def longestPalindrome(self, s):
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
         if not s:
             return ""
 
-        t = "^#" + "#".join(s) + "#$"
+        def expand_around_center(s: str, left: int, right: int):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return right - left - 1
 
-        n = len(t)
-        P = [0] * n
 
-        center = 0
-        right = 0
+        start = 0
+        end = 0
 
-        for i in range(1, n - 1):
-
-            mirror = 2 * center - i
-
-            if i < right:
-                P[i] = min(right - i, P[mirror])
-
-            while t[i + 1 + P[i]] == t[i - 1 - P[i]]:
-                P[i] += 1
-
-            if i + P[i] > right:
-                center = i
-                right = i + P[i]
-
-        max_len = max(P)
-        center_index = P.index(max_len)
-
-        start = (center_index - max_len) // 2
-
-        return s[start:start + max_len]
+        for i in range(len(s)):
+            odd = expand_around_center(s, i, i)
+            even = expand_around_center(s, i, i + 1)
+            max_len = max(odd, even)
+            
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+        
+        return s[start:end+1]
